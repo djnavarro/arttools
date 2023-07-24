3cbb3c46e61aa8e0d298afd006cda902ce4ab716
Danielle Navarro <djnavarro@protonmail.com>
2023-07-24 14:15:45 +1000

    paths behave correctly with NULL input

--- R/paths.R
+++ R/paths.R
@@ -1,5 +1,4 @@
 
-
 #' Paths to object buckets and repositories
 #'
 #' @param path Name of the series or a path to a subfolder within it
@@ -9,28 +8,32 @@
 #' @export
 bucket_remote_path <- function(path = NULL) {
   base <- getOption("arttools.bucket.remote")
-  paste(base, path, sep = "/")
+  if (!is.null(path)) base <- paste(base, path, sep = "/")
+  base
 }
 
 #' @rdname paths
 #' @export
 bucket_local_path <- function(path = NULL) {
   base <- getOption("arttools.bucket.local")
-  fs::path_expand(fs::path(base, path))
+  if (!is.null(path)) base <- fs::path(base, path)
+  fs::path_expand(base)
 }
 
 #' @rdname paths
 #' @export
 repo_remote_path <- function(path = NULL) {
   base <- getOption("arttools.repos.remote")
-  paste(base, path, sep = "/")
+  if (!is.null(path)) base <- paste(base, path, sep = "/")
+  base
 }
 
 #' @rdname paths
 #' @export
 repo_local_path <- function(path = NULL) {
   base <- getOption("arttools.repos.local")
-  fs::path_expand(fs::path(base, path))
+  if (!is.null(path)) base <- fs::path(base, path)
+  fs::path_expand(base)
 }
 
 
