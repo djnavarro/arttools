66d04ad669822725cbdebed1e3f4cfe3b038c786
Danielle Navarro <djnavarro@protonmail.com>
2023-07-24 12:31:07 +1000

    construct manifest tibble only at the end

--- R/manifest.R
+++ R/manifest.R
@@ -1,46 +1,54 @@
 
 #' Write a manifest file
 #'
-#' @param series_dir Path to the series directory
+#' @param series Path to the series directory
 #' @param date Publication date for the series
 #'
 #' @return A tibble
 #' @export
-create_manifest <- function(series_dir, date = Sys.Date()) {
+create_manifest <- function(series, date = Sys.Date()) {
 
-  series_name <- fs::path_split(series_dir)[[1]]
+  series_name <- fs::path_split(series)[[1]]
   series_name <- series_name[length(series_name)]
+  series_date <- date
 
-  manifest <- tibble::tibble()
-  manifest$series_name <- series_name
-  manifest$series_date <- date
-  manifest$path <- fs::dir_ls(series_dir, recurse = TRUE, regexp = "jpg$|png$")
-  manifest$folder = fs::path_dir(manifest$path)
-  manifest$file_name = fs::path_file(manifest$path)
-  manifest$format = fs::path_ext(manifest$path)
+  path <- fs::dir_ls(series, recurse = TRUE, regexp = "jpg$|png$")
+  folder = fs::path_dir(path)
+  file_name = fs::path_file(path)
+  file_format = fs::path_ext(path)
 
-  name_parts <- strsplit(manifest$file_name, "_")
+  name_parts <- strsplit(file_name, "_")
   num_name_parts <- vapply(name_parts, length, 1L)
   if (any(num_name_parts < 3L | num_name_parts > 4L)) {
     rlang::abort("image file names must have 3 or 4 parts")
   }
 
-  manifest$system_name <- vapply(name_parts, function(x) x[1], "")
-  manifest$system_version <- vapply(name_parts, function(x) x[2], "")
-  manifest$image_id <- vapply(name_parts, function(x) x[3], "")
-  manifest$image_short_title <- NA_character_
+  system_name <- vapply(name_parts, function(x) x[1], "")
+  system_version <- vapply(name_parts, function(x) x[2], "")
+  image_id <- vapply(name_parts, function(x) x[3], "")
+  image_short_title <- NA_character_
   titled_images <- num_name_parts == 4L
-  manifest$image_short_title[titled_images] <- vapply(
+  image_short_title[titled_images] <- vapply(
     name_parts[titled_images],
     function(x) x[4],
     ""
   )
 
+  manifest <- tibble::tibble(
+    series_name = series_name,
+    series_date = series_date,
+    path = path,
+    folder = folder,
+    file_name = file_name,
+    file_format = file_format,
+    system_name = system_name,
+    system_version = system_version,
+    image_id = image_id,
+    image_short_title = image_short_title,
+    manifest_version = 1L
+  )
   ord <- order(manifest$system_version, manifest$image_id)
   manifest <- manifest[ord, ]
-
-  manifest$manifest_version <- 1L
-
   #readr::write_csv(manifest, fs::path(dir, "manifest.csv"))
   manifest
 }
