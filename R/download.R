db7514bd42e982e91d8b8bd46b585a88f8ee85ae
Danielle Navarro <djnavarro@protonmail.com>
2023-07-24 14:17:01 +1000

    manifest_read and series_download use origin and destination arguments

--- R/download.R
+++ R/download.R
@@ -1,46 +1,37 @@
 #' Download an art series
 #'
 #' @param series Name of the series
-#' @param dest_dir Local folder into which the series folder is downloaded
-#' @param base_url URL for the bucket containing all art series
-#' @param keep_manifest Should the manifest file be downloaded? (default=TRUE)
+#' @param destination Local folder into which the series folder is downloaded
+#' @param origin Location in which to find the series
 #'
 #' @return Tibble containing the download status data
 #' @export
 series_download <- function(
     series,
-    dest_dir,
-    base_url = NULL,
-    keep_manifest = TRUE
+    destination = bucket_local_path(),
+    origin = bucket_remote_path()
 ) {
-  base_url <- get_base_url(base_url)
-  manifest <- read_manifest(series, base_url)
+  manifest <- manifest_read(series, origin)
   dirs <- unique(dirname(manifest$path))
-  fs::dir_create(fs::path(dest_dir, series, dirs))
-  file_paths <- manifest$path
-  if (keep_manifest) file_paths <- c("manifest.csv", file_paths)
+  fs::dir_create(fs::path(destination, series, dirs))
+  file_paths <- c("manifest.csv", manifest$path)
   curl::multi_download(
-    urls = paste(base_url, series, file_paths, sep = "/"),
-    destfiles = fs::path(dest_dir, series, file_paths)
+    urls = paste(origin, series, file_paths, sep = "/"),
+    destfiles = fs::path(destination, series, file_paths)
   )
 }
 
-#' Import the series manifest
+#' Read the series manifest
 #'
 #' @param series Name of the series
-#' @param base_url URL for the bucket containing all art series
+#' @param origin Location in which to find the series
 #'
 #' @return Tibble containing the manifest data
 #' @export
-read_manifest <- function(series, base_url = NULL) {
-  base_url <- get_base_url(base_url)
+manifest_read <- function(series, origin = bucket_remote_path()) {
   readr::read_csv(
-    paste(base_url, series, "manifest.csv", sep = "/"),
+    paste(origin, series, "manifest.csv", sep = "/"),
     show_col_types = FALSE
   )
 }
 
-get_base_url <- function(base_url) {
-  if (is.null(base_url)) return(getOption("arttools.base_url"))
-  base_url
-}
