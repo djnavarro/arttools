f064b807381e604725340615f920e15f317fa5a3
Danielle Navarro <djnavarro@protonmail.com>
2023-07-24 14:17:38 +1000

    resize takes an origin argument

--- R/image-resize.R
+++ R/image-resize.R
@@ -1,25 +1,29 @@
 
 #' Create resized copies of all images in a directory
 #'
-#' @param series Path to the series directory
-#' @param input_folder Folder within the series containing original images
-#' @param output_folder Folder within the series to contain resized images
+#' @param series Name of the series
+#' @param images_from Folder within the series containing original images
+#' @param images_to Folder within the series to contain resized images
 #' @param pixels_wide Width of output images in pixels
 #' @param pixels_high Height of output images in pixels description
+#' @param origin Location in which to find the series
 #'
 #' @return Invisibly returns NULL. This may change
 #' @export
 create_resized_images <- function(series,
-                                  input_folder,
-                                  output_folder,
+                                  images_from,
+                                  images_to,
                                   pixels_wide,
-                                  pixels_high = pixels_wide) {
-  image_file_names <- fs::path_file(fs::dir_ls(fs::path(series, input_folder)))
-  fs::dir_create(fs::path(series, output_folder))
-  lapply(image_file_names, function(image) {
+                                  pixels_high = pixels_wide,
+                                  origin = bucket_local_path()) {
+  base <- fs::path(origin, series)
+  file_paths <- fs::dir_ls(fs::path(base, images_from))
+  file_names <- fs::path_file(file_paths)
+  fs::dir_create(fs::path(base, images_to))
+  lapply(file_names, function(file) {
     create_resized_image(
-      fs::path(series, input_folder, image),
-      fs::path(series, output_folder, image),
+      fs::path(base, images_from, file),
+      fs::path(base, images_to, file),
       pixels_wide,
       pixels_high
     )
@@ -27,18 +31,18 @@ create_resized_images <- function(series,
   invisible(NULL)
 }
 
-create_resized_image <- function(input_image,
-                                 output_image,
+create_resized_image <- function(input_path,
+                                 output_path,
                                  pixels_wide,
                                  pixels_high = pixels_wide) {
   geometry <- magick::geometry_size_pixels(
     width = pixels_wide,
     height = pixels_wide
   )
-  img <- magick::image_read(input_image)
+  img <- magick::image_read(input_path)
   img <- magick::image_resize(img, geometry)
-  magick::image_write(img, path = output_image)
+  magick::image_write(img, path = output_path)
   rm(img)
   gc()
-  cli::cli_alert_success(paste(output_image, "created"))
+  cli::cli_alert_success(paste(output_path, "created"))
 }
