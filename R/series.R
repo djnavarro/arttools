edbdadba06117149b56035ff66de4dfdb7f999af
Danielle Navarro <djnavarro@protonmail.com>
2023-07-25 14:13:34 +1000

    adds series_check

--- R/series.R
+++ R/series.R
@@ -20,3 +20,47 @@ series_download <- function(
     destfiles = fs::path(destination, series, file_paths)
   )
 }
+
+#' Check the structure of an art series
+#'
+#' @param series Name of the series
+#' @param origin Location in which to find the series
+#'
+#' @return Invisibly returns TRUE if all checks pass, FALSE if at least one
+#' check fails
+#' @export
+series_check <- function(series, origin = bucket_local_path()) {
+  existence_ok <- series_check_exists(series, origin)
+  if (!existence_ok) return(invisible(FALSE))
+  manifest_ok <- series_check_manifest(series, origin)
+}
+
+series_check_exists <- function(series, origin) {
+  existence_ok <- fs::dir_exists(agnostic_path(origin, series))
+  if (!existence_ok) {
+    cli::cli_alert_info("No series folder detected")
+  } else {
+    cli::cli_alert_success("Series folder detected")
+  }
+  existence_ok
+}
+
+series_check_manifest <- function(series, origin) {
+  manifest_path <- agnostic_path(origin, series, "manifest.csv")
+  manifest_exists <- fs::file_exists(manifest_path)
+  if (!manifest_exists) {
+    cli::cli_alert_info("No manifest file detected")
+    return(FALSE)
+  }
+  cli::cli_alert_success("Manifest file detected")
+  manifest_from_file <- manifest_read(series, origin)
+  date <- manifest_from_file$series_date[1]
+  manifest_from_series <- manifest_build(series, date, origin)
+  comparison <- waldo::compare(manifest_from_file, manifest_from_series)
+  if (length(comparison) > 0) {
+    cli::cli_alert_info("Manifest file does not match images in series")
+    return(FALSE)
+  }
+  cli::cli_alert_success("Manifest file matches images in series")
+  TRUE
+}
