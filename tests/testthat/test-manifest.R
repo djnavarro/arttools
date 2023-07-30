85629cacbbeabe8f590f09b9b7538e6e50f64d10
Danielle Navarro <djnavarro@protonmail.com>
2023-07-30 15:19:08 +1000

    adds tests for manifest_build

---
+++ tests/testthat/test-manifest.R
@@ -0,0 +1,42 @@
+
+# setup -------------------------------------------------------------------
+
+temp_repos <- fs::path(tempdir(), "temp_repos")
+temp_bucket <- fs::path(tempdir(), "temp_bucket")
+
+if (fs::dir_exists(fs::path(temp_repos, "series-test"))) {
+  fs::dir_delete(fs::path(temp_repos, "series-test"))
+}
+
+fs::dir_create(fs::path(temp_bucket, "series-test"))
+fs::dir_create(fs::path(temp_bucket, "series-test", "image"))
+fs::dir_create(fs::path(temp_bucket, "series-test", "preview"))
+fs::file_create(fs::path(temp_bucket, "series-test", "image", "test_01_01.png"))
+fs::file_create(fs::path(temp_bucket, "series-test", "preview", "test_01_01.png"))
+
+# tests -------------------------------------------------------------------
+
+test_that("manifest_build creates manifest tibble", {
+
+  expect_no_error(manifest_build("series-test", Sys.Date(), temp_bucket))
+
+  manifest <- manifest_build("series-test", Sys.Date(), temp_bucket)
+  expect_s3_class(manifest, "data.frame")
+  expect_equal(nrow(manifest), 2)
+  expect_named(manifest, c("series_name", "series_date", "path", "folder",
+                           "file_name", "file_format", "system_name",
+                           "system_version", "image_id", "image_short_title",
+                           "image_long_title", "manifest_version"))
+
+})
+
+# clean up ----------------------------------------------------------------
+
+if (fs::dir_exists(fs::path(temp_bucket, "series-test"))) {
+  fs::dir_delete(fs::path(temp_bucket, "series-test"))
+}
+
+if (fs::dir_exists(fs::path(temp_repos, "series-test"))) {
+  fs::dir_delete(fs::path(temp_repos, "series-test"))
+}
+
